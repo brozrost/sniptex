16598e6bf497c2826bde178ea56cdd3b5052cd0b
Rostislav Brož <scraptechguy@gmail.com>
2026-04-24 11:42:33 +0200

    Create example2.py

---
+++ docs/example2.py
@@ -0,0 +1,35 @@
+class SniptexError(RuntimeError):
+    pass
+
+def extract_tagged_block(text: str, tag: str) -> str:
+    start_marker = f"sniptex-start {tag}"
+    end_marker = f"sniptex-end {tag}"
+
+    lines = text.splitlines()
+
+    start_index = None
+    end_index = None
+
+    # sniptex-start 1
+    for i, line in enumerate(lines):
+        if start_marker in line:
+            if start_index is not None:
+                raise SniptexError("Multiple start tags found for `{tag}`")
+
+            start_index = i
+
+    if start_index is None:
+        raise SniptexError("Start tag not found for `{tag}`")
+    # sniptex-end 1
+    
+    for i in range(start_index + 1, len(lines)):
+        if end_marker in lines[i]:
+            end_index = i
+            break
+
+    if end_index is None:
+        raise SniptexError("End tag not found for `{tag}`")
+    
+    # sniptex-start return
+    return "\n".join(lines[start_index + 1:end_index])
+    # sniptex-end return
