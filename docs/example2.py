85036bc7259f2ce1f5a3ceef98559ac7c674aaf6
Rostislav Brož <scraptechguy@gmail.com>
2026-06-30 10:39:12 +0200

    Rename

--- docs/example2.py
+++ docs/example2.py
@@ -1,28 +1,28 @@
 """
-sniptex-start comm0
-pip install sniptex
-sniptex-end comm0
+tagsnip-start comm0
+pip install tagsnip
+tagsnip-end comm0
 
-sniptex-start comm1
+tagsnip-start comm1
 \IncludeCode[options]{source}{tag}{language}{caption}
-sniptex-end comm1
+tagsnip-end comm1
 
-sniptex-start comm2
+tagsnip-start comm2
 \IncludeCode[firstnumber=1, fontsize=\scriptsize, style=monokai] {source}{tag}{language}{caption}
-sniptex-end comm2
+tagsnip-end comm2
 
-sniptex-start comm3
+tagsnip-start comm3
 \IncludeCode{example.py}{tag1}{Python}{Úryvek 2: ...}
-sniptex-end comm3
+tagsnip-end comm3
 
-sniptex-start comm4
-\IncludeCode{raw.githubusercontent.com/brozrost /sniptex/main/docs/example2.py}{tag2}{Python}{Úryvek 3: ...}
-sniptex-end comm4
+tagsnip-start comm4
+\IncludeCode{raw.githubusercontent.com/brozrost /tagsnip/main/docs/example2.py}{tag2}{Python}{Úryvek 3: ...}
+tagsnip-end comm4
 """
 
 import re
 
-class SniptexError(RuntimeError):
+class TagsnipError(RuntimeError):
     pass
 
 def marker_matches(line: str, marker: str) -> bool:
@@ -30,25 +30,25 @@ def marker_matches(line: str, marker: str) -> bool:
     return re.search(pattern, line) is not None
 
 def extract_tagged_block(text: str, tag: str):
-    start_marker = f"sniptex-start {tag}"
-    end_marker = f"sniptex-end {tag}"
+    start_marker = f"tagsnip-start {tag}"
+    end_marker = f"tagsnip-end {tag}"
 
     lines = text.splitlines()
 
     start_index = None
     end_index = None
 
-    # sniptex-start tag2
+    # tagsnip-start tag2
     for i, line in enumerate(lines):
         if marker_matches(line, start_marker):
             if start_index is not None:
-                raise SniptexError(f"Multiple start tags found for '{tag}'")
+                raise TagsnipError(f"Multiple start tags found for '{tag}'")
 
             start_index = i
 
     if start_index is None:
-        raise SniptexError(f"Start tag not found for '{tag}'")
-    # sniptex-end tag2
+        raise TagsnipError(f"Start tag not found for '{tag}'")
+    # tagsnip-end tag2
     
     for i in range(start_index + 1, len(lines)):
         if end_marker in lines[i]:
@@ -56,8 +56,8 @@ def extract_tagged_block(text: str, tag: str):
             break
 
     if end_index is None:
-        raise SniptexError(f"End tag not found for '{tag}'")
+        raise TagsnipError(f"End tag not found for '{tag}'")
     
-    # sniptex-start return
+    # tagsnip-start return
     return "\n".join(lines[start_index + 1:end_index]), start_index + 2, end_index
-    # sniptex-end return
+    # tagsnip-end return
